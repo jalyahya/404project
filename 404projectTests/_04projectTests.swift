cbb42debfc5ea1e7be18823a75e06cf2e068c071
jumana <jumana.alyahya@outlook.com>
2025-05-15 09:24:40 +0300

    Initial Commit

---
+++ 404projectTests/_04projectTests.swift
@@ -0,0 +1,17 @@
+//
+//  _04projectTests.swift
+//  404projectTests
+//
+//  Created by Jumana S on 15/05/2025.
+//
+
+import Testing
+@testable import _04project
+
+struct _04projectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ 404projectUITests/_04projectUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  _04projectUITestsLaunchTests.swift
+//  404projectUITests
+//
+//  Created by Jumana S on 15/05/2025.
+//
+
+import XCTest
+
+final class _04projectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
