5b124294ac8ce3a509033ccfeab0957459ad80ab
Reham <reretadeee87@gmail.com>
2025-05-23 22:08:21 +0300

    shathaa

--- 404/GameViewController.swift
+++ 404/GameViewController.swift
@@ -6,17 +6,32 @@
 //
 import UIKit
 import SpriteKit
+import GameplayKit
 
 class GameViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let view = self.view as? SKView {
-            view.ignoresSiblingOrder = true
-            
-            let scene = Start(size: view.bounds.size)
-            scene.scaleMode = .aspectFill
+        if let view = self.view as! SKView? {
+            let scene = GameScene(size: view.bounds.size)
+            scene.scaleMode = .resizeFill
             view.presentScene(scene)
+            view.ignoresSiblingOrder = true
+            view.showsFPS = true
+            view.showsNodeCount = true
         }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .landscape // نعرضها بشكل أفقي للآيباد
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
     }
 }
